907611edec0d317e621ffdf3cf8084f34bb6189b
Uduak Essien <acushla4real@gmail.com>
2021-09-24 10:46:55 +0100

    Initial setup with jumpstart template

--- config/application.rb
+++ config/application.rb
@@ -6,10 +6,10 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Jumpstart
+module Endless
   class Application < Rails::Application
     config.active_job.queue_adapter = :sidekiq
-    config.application_name = Rails.application.class.module_parent_name
+    config.application_name = 'Endless Cycle'
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
