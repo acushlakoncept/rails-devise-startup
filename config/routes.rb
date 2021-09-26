bafa8631fdf32183e010cf63b94597b58960f6cf
Uduak Essien <acushla4real@gmail.com>
2021-09-26 19:43:29 +0100

    Update routes with otp routes

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,15 @@ end
 
   resources :notifications, only: [:index]
   resources :announcements, only: [:index]
-  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
+  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks", sessions: "users/sessions" }
   root to: 'home#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resource :two_factor
+
+  devise_scope :user do 
+    scope :users, as: :users do 
+      post 'pre_otp', to: 'users/sessions#pre_otp'
+    end 
+  end
+
 end
